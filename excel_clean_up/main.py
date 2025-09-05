7a15d54e000a1b7182cb2468dcb1ce49945c4f5c
Yabtsega Aschalew <yabtsegaaschalew1@gmail.com>
2025-09-05 21:15:59 +0300

    complete validation logic functions

--- excel_clean_up/main.py
+++ excel_clean_up/main.py
@@ -1,5 +1,19 @@
 import pandas as pd
+#from .validation import validate_birthdate, validate_gender, validate_phone_number, validate_profession, validate_relation
 
-file = pd.read_excel('excel_file/Lideta SC 2nd round export_active_members_07-05-17.xlsx', sheet_name="W1")
-current_row = file.loc[100]
-print(current_row)
+
+xls = pd.ExcelFile('excel_file/Lideta SC 2nd round export_active_members_07-05-17.xlsx')
+
+
+print(xls.sheet_names)
+
+
+print("Number of sheets:", len(xls.sheet_names))
+
+
+headers = pd.read_excel(xls, sheet_name=xls.sheet_names[0], nrows=0).columns.tolist()
+print("Headers:", headers)
+
+df = pd.read_excel(xls, sheet_name=xls.sheet_names[0])  
+current_row = df.loc[0].tolist() 
+print("First row:", current_row)
--- excel_clean_up/validation.py
+++ excel_clean_up/validation.py
@@ -1,16 +1,100 @@
+from datetime import datetime
+from ethiopian_date import EthiopianDateConverter
 
-def validate_phone_number(string_val):
-    string_val = string_val.strip(" ")
+converter = EthiopianDateConverter()
+
+def validate_phone_number(string_val=None):
+    string_val = str(string_val.strip(" ").replace(" ", ""))
 
     if string_val.startswith("+251"):
-        print(string_val)  
+        return string_val
     elif string_val.startswith("0"):
         if len(string_val) == 10:
             formatted = "+251" + string_val[1:]  
-            print(formatted)
+            return formatted
         else:
-            print("Enter a valid number")
+            return ""
     else:
-        print("Invalid number")
+        return ""
 
-print("አ/ሰላም አ/ወሰዕ ሰኢድ")
+def validate_birthdate(birthdate=None):
+    # 03-03-1996
+    
+    if birthdate:
+        if "-" in birthdate:
+            # splitting birth date and reversing the list
+
+            birthdate = birthdate.split("-")[::-1]
+            gc_date = converter.to_gregorian(int(birthdate[0]), int(birthdate[1]), int(birthdate[2]))
+            return str(gc_date)
+        if "/" in birthdate:
+            # splitting birth date and reversing the list
+            birthdate = birthdate.split("/")[::-1]
+            gc_date = converter.to_gregorian(int(birthdate[0]), int(birthdate[1]), int(birthdate[2]))
+            return str(gc_date)
+    else:
+        current_date = datetime.now()
+        past_date = current_date.replace(year=current_date.year - 18)
+        return f"{past_date.year}-{past_date.month}-{past_date.day}"
+
+
+def validate_profession(profession=None):
+    profession_dict = {
+        "house wife": 1,
+        "employee": 2,
+        "self employed": 3,
+        "other": 4
+    }
+    
+    if not profession:
+        return profession_dict["other"]
+    
+    profession = " ".join(profession.split()).lower()
+    
+    for key in profession_dict:
+        if key in profession:
+            return profession_dict[key]
+    
+    return profession_dict["other"]
+
+def validate_relation(relation=None):
+    relation_dict = {
+        "self": "Head",
+        "sibling" : 1,
+        "parent": 2,
+        "relative": 3,
+        "child" : 4,
+        "grand_parents" : 5,
+        "employee" : 6,
+        "other" : 7,
+        "spouse": 8
+    }
+    if not relation:
+        return relation_dict["other"]
+    
+    relation = " ".join(relation.split()).lower()
+    
+    for key in relation_dict:
+        if key in relation_dict:
+            return relation_dict[key]
+    
+    return relation_dict["other"]
+
+
+def validate_gender(gender=None):
+    gender_dict = {
+        "Male":"M",
+        "Female": "F"
+    }
+    gender = " ".join(gender.split()).lower()
+
+    for key in gender_dict:
+        if key in gender_dict:
+            return gender_dict[key]
+        
+    if gender == "m":
+        return gender_dict["Male"]
+    elif gender == "f":
+        return gender_dict["Female"]
+    else:
+        return gender_dict["Male"]
